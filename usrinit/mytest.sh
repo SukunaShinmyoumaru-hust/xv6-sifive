f018b3d3fc99d4e6b23851276c8862d20b066fea
Cty <1416066155@qq.com>
2022-08-16 09:38:29 -0700

    allbad

--- usrinit/mytest.sh
+++ usrinit/mytest.sh
@@ -10,20 +10,20 @@
 ./test.sh sort.lua
 ./test.sh strings.lua
 ./busybox_testcode.sh
-./lmbench_testcode.sh
-echo "!TEST FINISH!"
-# lmbench_all lat_syscall -P 1 null
-# lmbench_all lat_syscall -P 1 read
-# lmbench_all lat_syscall -P 1 write
-# busybox mkdir -p /var/tmp
-# busybox touch /var/tmp/lmbench
-# lmbench_all lat_syscall -P 1 stat /var/tmp/lmbench
-# lmbench_all lat_syscall -P 1 fstat /var/tmp/lmbench
-# lmbench_all lat_syscall -P 1 open /var/tmp/lmbench
-# lmbench_all lat_select -n 100 -P 1 file
-# lmbench_all lat_sig -P 1 install
-# lmbench_all lat_sig -P 1 catch
-# lmbench_all lat_sig -P 1 prot lat_sig
+#./lmbench_testcode.sh
+echo latency measurements
+lmbench_all lat_sig -P 1 install
+lmbench_all lat_sig -P 1 catch
+lmbench_all lat_sig -P 1 prot lat_sig
+lmbench_all lat_syscall -P 1 null
+lmbench_all lat_syscall -P 1 read
+lmbench_all lat_syscall -P 1 write
+busybox mkdir -p /var/tmp
+busybox touch /var/tmp/lmbench
+lmbench_all lat_syscall -P 1 stat /var/tmp/lmbench
+lmbench_all lat_syscall -P 1 fstat /var/tmp/lmbench
+lmbench_all lat_syscall -P 1 open /var/tmp/lmbench
+lmbench_all lat_select -n 100 -P 1 file
 # lmbench_all lat_pipe -P 1
 # lmbench_all lat_proc -P 1 fork
 # lmbench_all lat_proc -P 1 exec
@@ -42,3 +42,4 @@ echo "!TEST FINISH!"
 # lmbench_all bw_mmap_rd -P 1 512k open2close /var/tmp/XXX
 # busybox echo context switch overhead
 # lmbench_all lat_ctx -P 1 -s 32 2 4 8 16 24 32 64 96
+echo "!TEST FINISH!"
