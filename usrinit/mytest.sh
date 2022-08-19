359845b5efef001cefbe4dbe130690b4a8692716
我永远喜欢少名针妙丸 <1416066155@qq.com>
2022-08-19 22:23:56 +0800

    Update mytest.sh

--- usrinit/mytest.sh
+++ usrinit/mytest.sh
@@ -33,14 +33,15 @@ lmbench_all lmdd label="File /var/tmp/XXX write bandwidth:" of=/var/tmp/XXX move
 
 busybox echo Bandwidth measurements
 lmbench_all bw_pipe -P 1
-# lmbench_all bw_file_rd -P 1 512k io_only /var/tmp/XXX
-lmbench_all bw_file_rd -P 1 512k open2close /var/tmp/XXX
+
 lmbench_all bw_mmap_rd -P 1 512k mmap_only /var/tmp/XXX
 lmbench_all bw_mmap_rd -P 1 512k open2close /var/tmp/XXX
 
 lmbench_all lat_mmap -P 1 512k /var/tmp/XXX
 busybox echo file system latency
 lmbench_all lat_fs /var/tmp
+lmbench_all bw_file_rd -P 1 512k io_only /var/tmp/XXX
+lmbench_all bw_file_rd -P 1 512k open2close /var/tmp/XXX
 lmbench_all lat_pagefault -P 1 /var/tmp/XXX
 busybox echo context switch overhead
 lmbench_all lat_ctx -P 1 -s 32 2 4 8 16 24 32 64 96
